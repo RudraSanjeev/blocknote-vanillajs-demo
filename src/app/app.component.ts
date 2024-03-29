d44f5e5bd507d4c1de65934666baa23494e93e7e
RudraSanjeev <sanjeev@rudrainnovative.in>
2024-03-29 18:04:27 +0530

    in and out nest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,27 @@ export class AppComponent {
 
     element.style.margin = '10px';
     element.style.textDecoration = 'none';
-    element.style.fontSize = '1.8rem';
+    element.style.fontSize = '1.2rem';
+    element.style.fontWeight = '300';
+    element.style.color = '#777';
+
+    if (onClick) {
+      element.addEventListener('click', (e) => {
+        onClick();
+        e.preventDefault();
+      });
+    }
+
+    return element;
+  }
+  createFormattingButton(text: string, onClick?: () => void) {
+    const element = document.createElement('a');
+    element.href = '#';
+    element.innerHTML = text;
+
+    element.style.margin = '10px';
+    element.style.textDecoration = 'none';
+    element.style.fontSize = '1rem';
     element.style.fontWeight = '600';
     element.style.color = '#999';
 
@@ -47,6 +67,51 @@ export class AppComponent {
 
     return element;
   }
+
+  // link btn
+  createLinkBtn(text: string, onLinkSet?: (url: string) => void) {
+    const element = document.createElement('button'); // Changed to button element
+    element.innerHTML = text;
+
+    element.style.margin = '10px';
+    element.style.fontSize = '1rem';
+    element.style.fontWeight = '600';
+    element.style.color = '#999';
+    element.style.border = 'none';
+    element.style.background = 'none';
+    element.style.cursor = 'pointer';
+
+    if (onLinkSet) {
+      element.addEventListener('click', (e) => {
+        e.preventDefault();
+        const span = document.createElement('span');
+        span.style.display = 'block';
+        span.style.marginTop = '10px';
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = 'Enter URL and press Enter';
+        input.style.width = '200px';
+        input.style.marginRight = '10px';
+        const button = document.createElement('button');
+        button.innerHTML = 'Cancel';
+        button.addEventListener('click', () => {
+          span.remove();
+        });
+        input.addEventListener('keyup', (e) => {
+          if (e.key === 'Enter') {
+            onLinkSet(input.value);
+            span.remove();
+          }
+        });
+        span.appendChild(input);
+        span.appendChild(button);
+        document.body.appendChild(span);
+      });
+    }
+
+    return element;
+  }
+
   createSelect(
     options: { text: string; value: string; propType?: number }[],
     onChange?: (value: string, propType?: number) => void
@@ -55,6 +120,7 @@ export class AppComponent {
     element.style.padding = '3px';
     element.style.background = 'transparent';
     element.style.outline = 'none';
+    element.style.border = 'none';
     element.style.fontSize = '1rem';
     element.style.fontWeight = '600';
     element.style.color = '#999';
@@ -62,6 +128,11 @@ export class AppComponent {
     options.forEach((option) => {
       const optionElement = document.createElement('option');
       optionElement.innerHTML = option.text;
+      optionElement.style.background = '#000';
+      optionElement.style.color = '#999';
+      optionElement.style.borderRadius = '15px';
+      optionElement.style.fontSize = '0.8rem';
+
       optionElement.value = option.value;
       if (option.propType !== undefined) {
         optionElement.setAttribute('data-propType', option.propType.toString());
@@ -71,11 +142,11 @@ export class AppComponent {
 
     if (onChange) {
       element.addEventListener('change', (event) => {
-        const selectedOption = options.find(
-          (option) => option.value === (event.target as HTMLSelectElement).value
-        );
-        const propType = selectedOption ? selectedOption.propType : undefined;
-        onChange((event.target as HTMLSelectElement).value, propType);
+        const selectedOption = event.target as HTMLSelectElement;
+        const value = selectedOption.value;
+        const propType =
+          selectedOption.selectedOptions[0]?.getAttribute('data-propType');
+        onChange(value, propType ? parseInt(propType, 10) : undefined);
       });
     }
 
@@ -105,18 +176,24 @@ export class AppComponent {
         sideMenuElement.style.justifyContent = 'center';
         sideMenuElement.style.position = 'absolute';
         sideMenuElement.style.top = '40px';
-        sideMenuElement.style.marginTop = '-5px';
-        // sideMenuElement.style.padding = '-5px ';
+        sideMenuElement.style.marginTop = '0px';
+        sideMenuElement.style.padding = '5px ';
         sideMenuElement.style.opacity = '0.8';
 
-        // Add button for side menu
-        const addBtn = this.createButton('+', () => {
-          editor.sideMenu.addBlock();
-        });
+        // Add button for side menu Plus
+        const addBtn = this.createButton(
+          '<i class="fa-solid fa-plus"></i>',
+          () => {
+            editor.sideMenu.addBlock();
+          }
+        );
         sideMenuElement.appendChild(addBtn);
 
         // Add button for dragging blocks
-        const dragBtn = this.createButton('::', () => {});
+        const dragBtn = this.createButton(
+          '<i class="fa-solid fa-grip-vertical"></i>',
+          () => {}
+        );
         dragBtn.addEventListener('dragstart', editor.sideMenu.blockDragStart);
         dragBtn.addEventListener('dragend', editor.sideMenu.blockDragEnd);
         dragBtn.draggable = true;
@@ -143,9 +220,9 @@ export class AppComponent {
         formattingToolbarElement.style.background = '#000';
         formattingToolbarElement.style.position = 'absolute';
         formattingToolbarElement.style.top = '50px';
-        formattingToolbarElement.style.padding = '3px';
-        formattingToolbarElement.style.opacity = '0.8';
-        formattingToolbarElement.style.border = '1px solid #ccc';
+        formattingToolbarElement.style.padding = '0';
+        // formattingToolbarElement.style.opacity = '0.8';
+        formattingToolbarElement.style.border = '0.5px solid #ccc';
         formattingToolbarElement.style.borderRadius = '10px';
         formattingToolbarElement.style.boxShadow =
           '1px 0px 8px -1px rgba(0,0,0,0.75);';
@@ -154,9 +231,9 @@ export class AppComponent {
         const changeParaSelect = this.createSelect(
           [
             { text: 'T Paragraph', value: 'paragraph' },
-            { text: 'T Heading 3', value: 'heading', propType: 3 },
             { text: 'T Heading 1', value: 'heading', propType: 1 },
             { text: 'T Heading 2', value: 'heading', propType: 2 },
+            { text: 'T Heading 3', value: 'heading', propType: 3 },
             { text: 'T Bullet list', value: 'bulletListItem' },
             { text: 'T Number list', value: 'numberedListItem' },
           ],
@@ -170,20 +247,6 @@ export class AppComponent {
           ) => {
             // console.log(value);
             const blockToUpdate = editor.getSelection().blocks[0].id;
-
-            // if (value === 'heading') {
-            //   console.log(propType);
-            //   editor.updateBlock(blockToUpdate, {
-            //     type: 'heading',
-            //     props: {
-            //       level: propType,
-            //     },
-            //   });
-            // } else {
-            //   editor.updateBlock(blockToUpdate, { type: value });
-            // }
-            console.log(value);
-            console.log(propType);
             if (propType !== undefined && propType !== null) {
               editor.updateBlock(blockToUpdate, {
                 type: 'heading',
@@ -201,7 +264,7 @@ export class AppComponent {
 
         // Add buttons for formatting options
         //bold
-        const boldBtn = this.createButton(
+        const boldBtn = this.createFormattingButton(
           '<i class="fa-solid fa-bold"></i>',
           () => {
             editor.toggleStyles({ bold: true });
@@ -210,7 +273,7 @@ export class AppComponent {
         );
         formattingToolbarElement.appendChild(boldBtn);
         // italicred
-        const italicBtn = this.createButton(
+        const italicBtn = this.createFormattingButton(
           '<i class="fa-solid fa-italic"></i>',
           () => {
             editor.toggleStyles({ italic: true });
@@ -218,7 +281,7 @@ export class AppComponent {
         );
         formattingToolbarElement.appendChild(italicBtn);
         // underline
-        const underlineBtn = this.createButton(
+        const underlineBtn = this.createFormattingButton(
           '<i class="fa-solid fa-underline"></i>',
           () => {
             editor.toggleStyles({ underline: true });
@@ -226,7 +289,7 @@ export class AppComponent {
         );
         formattingToolbarElement.appendChild(underlineBtn);
         // strike through
-        const strikethroughBtn = this.createButton(
+        const strikethroughBtn = this.createFormattingButton(
           '<i class="fa-solid fa-strikethrough"></i>',
           () => {
             editor.toggleStyles({ strike: true });
@@ -236,7 +299,7 @@ export class AppComponent {
         formattingToolbarElement.appendChild(strikethroughBtn);
 
         // align left btn
-        const alignLeftBtn = this.createButton(
+        const alignLeftBtn = this.createFormattingButton(
           '<i class="fa-solid fa-align-left"></i>',
           () => {
             console.log(editor.getSelection().blocks);
@@ -248,7 +311,7 @@ export class AppComponent {
         );
         formattingToolbarElement.appendChild(alignLeftBtn);
         // align center btn
-        const alignCenterBtn = this.createButton(
+        const alignCenterBtn = this.createFormattingButton(
           '<i class="fa-solid fa-align-center"></i>',
           () => {
             console.log(editor.getSelection().blocks);
@@ -260,7 +323,7 @@ export class AppComponent {
         );
         formattingToolbarElement.appendChild(alignCenterBtn);
         // align right btn
-        const alignRightBtn = this.createButton(
+        const alignRightBtn = this.createFormattingButton(
           '<i class="fa-solid fa-align-right"></i>',
           () => {
             console.log(editor.getSelection().blocks);
@@ -275,9 +338,15 @@ export class AppComponent {
         const colorSelect = this.createSelect(
           [
             { text: 'A', value: '' },
-            { text: 'A Red', value: 'red' },
-            { text: 'A Green', value: 'green' },
-            { text: 'A Blue', value: 'blue' },
+            { text: 'Gray', value: 'gray' },
+            { text: 'Brown', value: 'brown' },
+            { text: 'Red', value: 'red' },
+            { text: 'Orange', value: 'orange' },
+            { text: 'Yellow', value: 'yellow' },
+            { text: 'Green', value: 'green' },
+            { text: 'Blue', value: 'blue' },
+            { text: 'Purple', value: 'purple' },
+            { text: 'Pink', value: 'pink' },
           ],
           (value) => {
             editor.toggleStyles({ textColor: value });
@@ -285,6 +354,41 @@ export class AppComponent {
         );
 
         formattingToolbarElement.appendChild(colorSelect);
+
+        // set link
+        const setLink = this.createLinkBtn(
+          '<i class="fa-solid fa-link"></i>',
+          (url) => {
+            console.log(url);
+            const text = editor.getSelectedText();
+            editor.createLink(url, text);
+          }
+        );
+        formattingToolbarElement.appendChild(setLink);
+        // nest block
+        const setNest = this.createFormattingButton(
+          '<i class="fa-solid fa-indent"></i>',
+          () => {
+            // const currentBlock = editor.getSelection().blocks[0];
+
+            const canNest = editor.canNestBlock();
+            if (canNest) {
+              editor.nestBlock();
+            }
+            console.log(editor.nestBlock());
+          }
+        );
+        formattingToolbarElement.appendChild(setNest);
+        // unnest block
+        const setUnNest = this.createFormattingButton(
+          '<i class="fa-solid fa-outdent"></i>',
+          () => {
+            // const currentBlock = editor.getSelection().blocks[0];
+            editor.unnestBlock();
+            // console.log(editor.nestBlock());
+          }
+        );
+        formattingToolbarElement.appendChild(setUnNest);
 
         document.getElementById('root')!.appendChild(formattingToolbarElement);
       }
@@ -297,5 +401,11 @@ export class AppComponent {
         formattingToolbarElement.style.display = 'none';
       }
     });
+
+    // editor.suggestionMenus.onUpdate((suggestionMenusState) =>{
+    //   if(!suggestionMenuElement){
+
+    //   }
+    // })
   }
 }
